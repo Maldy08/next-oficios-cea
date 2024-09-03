889c411acbdc70f114c330b66e6d26c9841dcf44
Crono115 <cronot58@gmail.com>
2024-09-03 11:27:55 -0700

    NDSKLF

--- src/app/oficios/oficios-expedidos/components/components table/ModalPersonaEnvio.tsx
+++ src/app/oficios/oficios-expedidos/components/components table/ModalPersonaEnvio.tsx
@@ -1,39 +1,40 @@
 import { FC, useState, useEffect } from "react";
-import { FaSearch, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import axios from 'axios';
+import { FaSearch, FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import axios from "axios";
 
 interface ModalPersonaEnvioProps {
   isOpen: boolean;
   onClose: () => void;
   onSave: (name: string) => void;
+  datosEmpleados: Empleados[];
 }
 
-const ModalPersonaEnvio: FC<ModalPersonaEnvioProps> = ({ isOpen, onClose, onSave }) => {
+interface Empleados {
+  nombreCompleto: string;
+  descripcionDepto: string;
+  descripcionPuesto: string;
+  idPue: number;
+}
+
+const ModalPersonaEnvio: FC<ModalPersonaEnvioProps> = ({
+  isOpen,
+  onClose,
+  onSave,
+  datosEmpleados,
+}) => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [selectedPersona, setSelectedPersona] = useState<string | null>(null);
   const [data, setData] = useState<any[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('/api/empleados');
-        console.log('API Response:', response.data.data);
-        setData(response.data.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   const handleChangePage = (newPage: number) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
@@ -43,7 +44,7 @@ const ModalPersonaEnvio: FC<ModalPersonaEnvioProps> = ({ isOpen, onClose, onSave
     setPage(0);
   };
 
-  const handleRowClick = (persona: { nombreCompleto: string; }) => {
+  const handleRowClick = (persona: { nombreCompleto: string }) => {
     setSelectedPersona(persona.nombreCompleto);
   };
 
@@ -54,20 +55,30 @@ const ModalPersonaEnvio: FC<ModalPersonaEnvioProps> = ({ isOpen, onClose, onSave
     }
   };
 
-  const filteredData = data.filter(row =>
-    row.nombreCompleto.toLowerCase().includes(searchText.toLowerCase()) ||
-    row.descripcionDepto.toLowerCase().includes(searchText.toLowerCase()) ||
-    row.descripcionPuesto.toLowerCase().includes(searchText.toLowerCase())
+  const filteredData = datosEmpleados.filter(
+    (row) =>
+      row.nombreCompleto.toLowerCase().includes(searchText.toLowerCase()) ||
+      row.descripcionDepto.toLowerCase().includes(searchText.toLowerCase()) ||
+      row.descripcionPuesto.toLowerCase().includes(searchText.toLowerCase())
   );
 
   const totalPages = Math.ceil(filteredData.length / rowsPerPage);
 
   return (
-    <div className={`fixed inset-0 flex items-center justify-center z-50 overflow-y-auto ${isOpen ? 'block' : 'hidden'}`}>
-      <div className="fixed inset-0 bg-black bg-opacity-50" aria-hidden="true"></div>
+    <div
+      className={`fixed inset-0 flex items-center justify-center z-50 overflow-y-auto ${
+        isOpen ? "block" : "hidden"
+      }`}
+    >
+      <div
+        className="fixed inset-0 bg-black bg-opacity-50"
+        aria-hidden="true"
+      ></div>
       <div className="bg-white w-full max-w-4xl h-[80vh] max-h-[600px] p-6 rounded-lg shadow-lg relative flex flex-col z-10">
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4">
-          <h2 className="text-lg font-semibold mb-2 sm:mb-0">Seleccionar Persona de Envío</h2>
+          <h2 className="text-lg font-semibold mb-2 sm:mb-0">
+            Seleccionar Persona de Envío
+          </h2>
           <div className="relative w-full max-w-[300px]">
             <input
               type="text"
@@ -84,23 +95,35 @@ const ModalPersonaEnvio: FC<ModalPersonaEnvioProps> = ({ isOpen, onClose, onSave
           <table className="w-full border-collapse">
             <thead>
               <tr>
-                <th className="font-bold border-b py-2 px-4">NOMBRE COMPLETO</th>
+                <th className="font-bold border-b py-2 px-4">
+                  NOMBRE COMPLETO
+                </th>
                 <th className="font-bold border-b py-2 px-4">DEPARTAMENTO</th>
                 <th className="font-bold border-b py-2 px-4">CARGO</th>
               </tr>
             </thead>
             <tbody>
-              {filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
-                <tr 
-                  key={index} 
-                  onClick={() => handleRowClick(row)}
-                  className={`cursor-pointer ${selectedPersona === row.nombreCompleto ? 'bg-blue-100' : ''}`}
-                >
-                  <td className="border-b py-2 px-4">{row.nombreCompleto}</td>
-                  <td className="border-b py-2 px-4">{row.descripcionDepto}</td>
-                  <td className="border-b py-2 px-4">{row.descripcionPuesto}</td>
-                </tr>
-              ))}
+              {filteredData
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => (
+                  <tr
+                    key={row.idPue}
+                    onClick={() => handleRowClick(row)}
+                    className={`cursor-pointer ${
+                      selectedPersona === row.nombreCompleto
+                        ? "bg-blue-100"
+                        : ""
+                    }`}
+                  >
+                    <td className="border-b py-2 px-4">{row.nombreCompleto}</td>
+                    <td className="border-b py-2 px-4">
+                      {row.descripcionDepto}
+                    </td>
+                    <td className="border-b py-2 px-4">
+                      {row.descripcionPuesto}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
@@ -117,7 +140,9 @@ const ModalPersonaEnvio: FC<ModalPersonaEnvioProps> = ({ isOpen, onClose, onSave
             </button>
             <button
               type="button"
-              onClick={() => handleChangePage(Math.min(totalPages - 1, page + 1))}
+              onClick={() =>
+                handleChangePage(Math.min(totalPages - 1, page + 1))
+              }
               className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300"
               disabled={page >= totalPages - 1}
             >
@@ -126,7 +151,11 @@ const ModalPersonaEnvio: FC<ModalPersonaEnvioProps> = ({ isOpen, onClose, onSave
           </div>
           <div className="flex items-center space-x-2">
             <span className="text-sm">Folios por pág:</span>
-            <select value={rowsPerPage} onChange={handleChangeRowsPerPage} className="border border-gray-300 rounded px-2 py-1 text-sm">
+            <select
+              value={rowsPerPage}
+              onChange={handleChangeRowsPerPage}
+              className="border border-gray-300 rounded px-2 py-1 text-sm"
+            >
               <option value={5}>5</option>
               <option value={10}>10</option>
             </select>
--- src/app/oficios/oficios-expedidos/page.tsx
+++ src/app/oficios/oficios-expedidos/page.tsx
@@ -1,16 +1,22 @@
-import TableComponent from './components/table';
-import ClientComponent from './components/ClientComponent';
+import TableComponent from "./components/table";
+import ClientComponent from "./components/ClientComponent";
 
 export default async function OficiosExpedidosPage() {
   // Hacer la llamada a la API para obtener los oficios
-  const responseOficios = await fetch('http://localhost:3000/api/oficios');
+  const responseOficios = await fetch("http://localhost:3000/api/oficios");
   const dataOficios = await responseOficios.json();
   const rows = dataOficios.data || []; // Datos de oficios para la tabla
 
   // Hacer la llamada a la API para obtener los departamentos
-  const responseDepartamentos = await fetch('http://localhost:3000/api/departamentos');
+  const responseDepartamentos = await fetch(
+    "http://localhost:3000/api/departamentos"
+  );
   const dataDepartamentos = await responseDepartamentos.json();
   const departamentos = dataDepartamentos.data || []; // Datos de departamentos para el modal
+
+  const empleados = await fetch("http://localhost:3000/api/empleados");
+  const empleados2 = await empleados.json();
+  const datosEmpleados = empleados2.data || [];
 
   // Renderizar el componente servidor con los datos obtenidos
   return (
@@ -18,7 +24,11 @@ export default async function OficiosExpedidosPage() {
       <h1 className="text-lg font-bold mb-4">Oficios-Expedidos</h1>
 
       {/* Renderizamos el componente cliente para manejar la lógica de botones y búsqueda */}
-      <ClientComponent rows={rows} departamentos={departamentos} />
+      <ClientComponent
+        rows={rows}
+        departamentos={departamentos}
+        datosEmpleados={datosEmpleados}
+      />
     </div>
   );
 }
