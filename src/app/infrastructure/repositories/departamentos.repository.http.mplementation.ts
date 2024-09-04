115a8c5e926038db04e3c37fd03f6df9ac82b188
Crono115 <cronot58@gmail.com>
2024-09-04 12:49:33 -0700

    Prueba1

--- src/app/infrastructure/repositories/departamentos.repository.http.mplementation.ts
+++ src/app/infrastructure/repositories/departamentos.repository.http.mplementation.ts
@@ -1,18 +1,19 @@
-import { DepartamentosRepository } from "@/app/application/interfaces/departamentos-repository";
+import { DepartamentosRepository } from "@/app/application/interfaces/departamentos.interface.repository";
 import { Departamentos } from "@/app/domain/entities";
 import { DbAdapter } from "../adapters/db.adapter";
 import { Result } from "@/app/domain/common/result";
 import { DepartamentosMapper } from "../mappers/departamentos.mapper";
 
+export class DepartamentosRepositoryHttpImplementation
+  implements DepartamentosRepository
+{
+  async getAllDepartamentos(): Promise<Departamentos[]> {
+    const { data } = await DbAdapter.get<Result<Departamentos[]>>(
+      "departamentos"
+    );
 
-export class DepartamentosRepositoryHttpImplementation implements DepartamentosRepository  {
-
-   async getAllDepartamentos(): Promise<Departamentos[]> {
-        const { data } = await DbAdapter.get<Result<Departamentos[]>>(
-            "departamentos"
-          );
-
-            return data.map((departamento) => DepartamentosMapper.mapFromApiToDomain(departamento));
-    }
-
+    return data.map((departamento) =>
+      DepartamentosMapper.mapFromApiToDomain(departamento)
+    );
+  }
 }
