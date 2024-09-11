e55f58066a9a59f3d1b708356aa03b9ad674e037
Crono115 <168789614+Crono115@users.noreply.github.com>
2024-09-11 12:10:58 -0700

    Alejandro2

--- src/app/infrastructure/repositories/departamentos.repository.http.mplementation.ts
+++ src/app/infrastructure/repositories/departamentos.repository.http.mplementation.ts
@@ -2,20 +2,19 @@ import { DepartamentosRepository } from "@/app/application/interfaces/departamen
 import { Departamentos } from "@/app/domain/entities";
 import { DbAdapter } from "../adapters/db.adapter";
 import { Result } from "@/app/domain/common/result";
-<<<<<<< HEAD
-import {
-  DepartamentosMapper,
-  EmpleadosMapper,
-} from "../mappers/departamentos.mapper";
+import { DepartamentosMapper } from "../mappers/departamentos.mapper";
 
 export class DepartamentosRepositoryHttpImplementation
   implements DepartamentosRepository
 {
   async getAllDepartamentos(): Promise<Departamentos[]> {
     try {
+      // Realiza la solicitud a la API utilizando DbAdapter
       const { data } = await DbAdapter.get<Result<Departamentos[]>>(
         "departamentos"
-      ); // Ruta correcta
+      );
+
+      // Mapea los datos obtenidos del API al dominio
       return data.map((departamento) =>
         DepartamentosMapper.mapFromApiToDomain(departamento)
       );
@@ -24,21 +23,4 @@ export class DepartamentosRepositoryHttpImplementation
       throw new Error("Error fetching departamentos from repository");
     }
   }
-=======
-import { DepartamentosMapper } from "../mappers/departamentos.mapper";
-
-export class DepartamentosRepositoryHttpImplementation implements DepartamentosRepository {
-    async getAllDepartamentos(): Promise<Departamentos[]> {
-        try {
-            // Realiza la solicitud a la API utilizando DbAdapter
-            const { data } = await DbAdapter.get<Result<Departamentos[]>>("departamentos");
-
-            // Mapea los datos obtenidos del API al dominio
-            return data.map(departamento => DepartamentosMapper.mapFromApiToDomain(departamento));
-        } catch (error) {
-            console.error("Error fetching departamentos from repository:", error);
-            throw new Error("Error fetching departamentos from repository");
-        }
-    }
->>>>>>> roberto
 }
